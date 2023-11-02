eb5e5d7327809e880ca23fc5f5c83b9d9ced1890
rivkyschon <113901370+rivkyschon@users.noreply.github.com>
2023-11-02 17:28:54 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,6 @@
 echo "Hello World !!!!"
-docker-compose up
+ls
+docker-compose up --build
 docker ps
 #docker run -d -p 27017:27017 --name mongodb -v mongodb-data:/data/db mongo:latest
 #docker run -d -p 4000:4000 --name nodejs-app -v mongodb-data:/data/db rivkyschon/nodejs-app:v0.0.1
