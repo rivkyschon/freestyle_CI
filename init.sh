7b1209b235dc8acac34a9ac1f4d06f1d1a6cc02c
rivkyschon <113901370+rivkyschon@users.noreply.github.com>
2023-11-02 16:43:40 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 echo "Hello World !!!!"
-#docker-compose up
+docker-compose up
 docker ps
-docker run -d -p 27017:27017 --name mongodb -v mongodb-data:/data/db mongo:latest
-docker run -d -p 4000:4000 --name nodejs-app -v mongodb-data:/data/db rivkyschon/nodejs-app:v0.0.1
+#docker run -d -p 27017:27017 --name mongodb -v mongodb-data:/data/db mongo:latest
+#docker run -d -p 4000:4000 --name nodejs-app -v mongodb-data:/data/db rivkyschon/nodejs-app:v0.0.1
