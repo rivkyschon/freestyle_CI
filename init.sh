c6ff2367b11b58cb94609f51f3066e61fd724824
rivkyschon <113901370+rivkyschon@users.noreply.github.com>
2023-11-02 15:55:31 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,3 +1,5 @@
 echo "Hello World !!!!"
 #docker-compose up
 docker ps
+docker run -d -p 27017:27017 --name mongodb -v mongodb-data:/data/db mongo:latest
+docker run -d -p 27017:27017 --name mongodb -v mongodb-data:/data/db mongo:latest
