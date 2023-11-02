50cc5c097f6953bebdf9f0840baff1e2994aa671
rivkyschon <113901370+rivkyschon@users.noreply.github.com>
2023-11-03 01:55:23 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,6 +1,13 @@
 echo "Hello World !!!!"
 ls
-docker-compose up --build
-docker ps
+#docker-compose up --build
+#docker ps
 #docker run -d -p 27017:27017 --name mongodb -v mongodb-data:/data/db mongo:latest
 #docker run -d -p 4000:4000 --name nodejs-app -v mongodb-data:/data/db rivkyschon/nodejs-app:v0.0.1
+docker network create my-network
+
+docker run -d -p 27017:27017 --name mongodb -v mongodb-data:/data/db --network my-network mongo:latest
+
+docker build -t rivkyschon/nodejs-app:ci .
+
+docker run -d -p 4000:4000 --name nodejs-app -v mongodb-data:/data/db --network my-network rivkyschon/nodejs-app:ci
