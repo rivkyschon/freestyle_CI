347dd309215c3931dd4eb5c97adc93ec3d650a91
rivkyschon <113901370+rivkyschon@users.noreply.github.com>
2023-11-03 02:31:06 +0200

    Update and rename init.sh to start-containers.sh

---
+++ start-containers.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Define your custom network name
+NETWORK_NAME="my-network"
+
+# Stop and remove the existing containers
+docker stop mongodb nodejs-app
+docker rm mongodb nodejs-app
+
+# Check if the custom network exists; create it if not
+if ! docker network inspect "$NETWORK_NAME" &> /dev/null; then
+  docker network create "$NETWORK_NAME"
+fi
+
+# Run the MongoDB container
+docker run -d -p 27017:27017 --name mongodb -v mongodb-data:/data/db --network "$NETWORK_NAME" mongo:latest
+
+# Build and run the Node.js container
+docker build -t rivkyschon/nodejs-app:ci .
+docker run -d -p 4000:4000 --name nodejs-app -v mongodb-data:/data/db --network "$NETWORK_NAME" rivkyschon/nodejs-app:ci
